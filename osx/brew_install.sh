e573fccd4c9c7baa71ea0af38c9932d969f11c40
Egor Orlov <egor.orlov@ostrovok.ru>
2014-11-05 23:42:13 +0300

    added brew install

---
+++ osx/brew_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+brew install openssl
+brew install redis
+brew install postgresql
+
+# Install Cask
+brew install caskroom/cask/brew-cask
+
+# Install Casks
+brew cask install alfred
+brew cask install flux
+
+brew cask install virtualbox
+brew cask install node
+
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install atom
+
+brew cask install adium
+brew cask install nvalt
+brew cask install rdio
