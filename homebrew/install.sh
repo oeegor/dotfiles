90bb51e099031b4f2a6e12168b051c0717ea05b3
orlovegor <e.k.orlov@gmail.com>
2014-12-07 09:13:41 +0300

    cleanup

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -14,5 +14,27 @@ fi
 
 # Install homebrew packages
 brew install grc coreutils spark
+brew install openssl
+brew install redis
+brew install postgresql
+brew install htop
+
+# Install Cask
+brew install caskroom/cask/brew-cask
+
+# Install Casks
+brew cask install alfred
+brew cask install flux
+
+brew cask install virtualbox
+brew cask install node
+
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install atom
+
+brew cask install nvalt
+brew cask install rdio
+brew cask install audacity
 
 exit 0
