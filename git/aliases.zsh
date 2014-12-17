2c4b3b0bc7f69c3e528fee7d7fd9409a6afc6226
orlovegor <e.k.orlov@gmail.com>
2014-12-17 21:40:38 +0300

    groot

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -19,3 +19,4 @@ alias gco='git checkout'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias groot='cd "$(git rev-parse --show-toplevel)"'  # go to the root of the project
