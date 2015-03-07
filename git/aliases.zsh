44be53cff39a9172e99ac66156c533f6ebd7946e
Egor Orlov <egor.orlov@ostrovok.ru>
2015-03-07 23:07:32 +0300

    better git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -14,9 +14,10 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
-alias gca='git commit -a'
+alias gcm='git checkout master'
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
+alias gcb='git checkout -b'
+alias gbn='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias groot='cd "$(git rev-parse --show-toplevel)"'  # go to the root of the project
