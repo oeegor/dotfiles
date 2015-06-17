36496bb2af321b84af004a7d2470c70467abda41
Egor Orlov <egor.orlov@ostrovok.ru>
2015-06-17 19:50:36 +0300

    emake

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -23,4 +23,3 @@ alias gbn='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias groot='cd "$(git rev-parse --show-toplevel)"'  # go to the root of the project
-alias emake='env -i make'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,2 @@
 alias reload!='. ~/.zshrc'
+alias emake='env -i make'
