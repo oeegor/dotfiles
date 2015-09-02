c92d4e19c46575b2e717e7688002c4d9ba9d47f3
Egor Orlov <egor.orlov@ostrovok.ru>
2015-09-02 14:25:53 +0300

    n

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -7,7 +7,7 @@ debash() {
     docker exec -it "$*" bash
 }
 
-alias dcu=docker-compose up
-alias dcb=docker-compose build
-alias dcs=docker-compose stop
-alias dcp=docker-compose ps
+alias dcu="docker-compose up"
+alias dcb="docker-compose build"
+alias dcs="docker-compose stop"
+alias dcp="docker-compose ps"
