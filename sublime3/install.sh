d779ae73c51ea3e4f15fd26d461bdbada0f9f3b1
Egor Orlov <egor.orlov@ostrovok.ru>
2016-02-03 23:56:59 +0300

    go

--- sublime3/install.sh
+++ sublime3/install.sh
@@ -1,18 +1,20 @@
+#!/bin/bash
+
 TARGET_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 INSTALLED_PKGS_DIR="$HOME/Library/Application Support/Sublime Text 3/Installed Packages/"
-PACKAGE_MANAGER_APP_PATH="${INSTALLED_PKGS_DIR}Package Control.sublime-package"
+PACKAGE_MANAGER_APP_PATH="$INSTALLED_PKGS_DIRPackage Control.sublime-package"
 
 echo "copying sublime3 config"
-cp sublime3/Preferences.sublime-settings "$TARGET_DIR"
+cp sublime3/Preferences.sublime-settings $TARGET_DIR
 
 echo "copying sublime3 key bindings"
-cp "sublime3/Default (OSX).sublime-keymap" "$TARGET_DIR"
+cp "sublime3/Default (OSX).sublime-keymap" $TARGET_DIR
 
 echo "installing package manager"
-if [ -f "${INSTALLED_PKGS_DIR}/Package Control.sublime-package" ]
+if [ -f "$INSTALLED_PKGS_DIR/Package Control.sublime-package" ]
 then
   echo "package manager is already installed"
 else
-  curl -o "$PACKAGE_MANAGER_APP_PATH" \
+  curl -o $PACKAGE_MANAGER_APP_PATH \
   https://packagecontrol.io/Package%20Control.sublime-package
 fi
