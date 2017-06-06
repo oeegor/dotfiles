3e9718eb97588e5eb767a8c952f19ce85786bd4d
Egor Orlov <egor.orlov@ostrovok.ru>
2017-06-06 19:22:53 +0300

    [master]

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -12,5 +12,10 @@ main = do
         { modMask       = mod4Mask
         , terminal      = "termite"
         , logHook = dynamicLogWithPP $ sjanssenPP {ppOutput = hPutStrLn xmproc}
-
+        , startupHook = startup
         }
+
+startup :: X()
+startup = do
+    spawn "trayer --edge bottom --align right --SetDockType true --SetPartialStrut true --expand true --widthtype percent --width 100 --height 25"
+    spawn "nm-applet"
