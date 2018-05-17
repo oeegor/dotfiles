099db53ae3b1879c7a32de2a33f720095dda72b6
Egor Orlov <egor.orlov@ostrovok.ru>
2018-05-17 09:37:02 +0300

    [master]

--- system/path.zsh
+++ system/path.zsh
@@ -1,4 +1,4 @@
-export GOPATH="$HOME/dev/go"
+export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:$PATH:$GOBIN"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
