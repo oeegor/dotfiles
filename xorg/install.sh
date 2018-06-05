6e8a8c4da06dc755d55d97eaf370f05d108ed765
Egor Orlov <oeegor@gmail.com>
2018-06-05 08:45:05 +0300

    [master] redshift brightness

--- xorg/install.sh
+++ xorg/install.sh
@@ -5,5 +5,6 @@ set -ex
 if [ "$(uname -s)" == "Linux" ]
 then
 	sudo ln -fs $(pwd)/xorg.conf.d/* /etc/X11/xorg.conf.d/
-
+	mkdir -p ~/.config/redshift/hooks
+	sudo ln -fs $(pwd)/redshift_brightness.sh ~/.config/redshift/hooks/brightness.sh
 fi
