de94d991ec7966101139b118a6a1851053e0ee37
Egor Orlov <oeegor@protonmail.com>
2019-04-02 11:15:07 +0300

    switch to app && bind keys fix

---
+++ xorg/switch_to_app.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# What it does:
+
+#     gets the current desktop number
+#     searches current desktop for the given name (parameter one)
+
+#     then, depending on the result:
+#         either switches to found app
+#         or launches given app (parameter two)
+
+# Usage (expecting the script name to be switch_to_app:
+
+# switch_to_app LookForThisString LaunchThisIfNotFound
+
+# eg
+
+# switch_to_app Chromium chromium-browser
+
+# EDIT: more awesome version - when you launch the command again (eg press the keystroke again), it cycles to another instance of the window
+# source: https://askubuntu.com/questions/272566/wmctrl-ignore-other-workspaces/274231#274231
+
+app_name=$1
+workspace_number=`wmctrl -d | grep '\*' | cut -d' ' -f 1`
+win_list=`wmctrl -lx | grep $app_name | grep " $workspace_number " | awk '{print $1}'`
+
+active_win_id=`xprop -root | grep '^_NET_ACTIVE_W' | awk -F'# 0x' '{print $2}' | awk -F', ' '{print $1}'`
+if [ "$active_win_id" == "0" ]; then
+    active_win_id=""
+fi
+
+# get next window to focus on, removing id active
+switch_to=`echo $win_list | sed s/.*$active_win_id// | awk '{print $1}'`
+# if the current window is the last in the list ... take the first one
+if [ "$switch_to" == "" ];then
+    switch_to=`echo $win_list | awk '{print $1}'`
+fi
+
+if [[ -n "${switch_to}" ]]
+    then
+        (wmctrl -ia "$switch_to") &
+    else
+        if [[ -n "$2" ]]
+            then
+                ($2) &
+        fi
+fi
+
+exit 0
