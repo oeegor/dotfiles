0f2acbefcc610a70cde0905e3972df93bad996fa
Egor Orlov <oeegor@gmail.com>
2019-07-20 09:19:51 +0300

    [master] pacman -S && usbwakeup

--- arch/install.sh
+++ arch/install.sh
@@ -4,33 +4,36 @@ set -ex
 
 if [ "$(uname -s)" == "Linux" ]
 then
-	sudo pacman -Sy --noconfirm \
-		--needed base-devel \
-		alsa-utils \
-		bluez \
-		bluez-utils \
-		chromium \
-		dmenu \
-		git \
-		htop \
-		libinput \
-		maim xclip \
-		netcat \
-		openvpn \
-		python-pip \
-		sudo \
-		termite \
-		unzip \
-		ttf-bitstream-vera \
-		xmobar \
-		xmonad \
-		xmonad-contrib \
-		xorg-server \
-		xorg-xdpyinfo \
-		xorg-xinit \
-		xorg-xinput \
-		xorg-xrandr \
-		xterm \
-		vim \
-		zsh
+    sudo pacman -Sy --noconfirm \
+        --needed base-devel \
+                bind-tools \  # for host, nslookup
+        chromium \
+        firefox \
+        git \
+        gnome-keyring \  # for mailspring to work
+        gvfs \  # to support trash and others. https://wiki.gnome.org/Projects/gvfs
+        htop \
+        iotop \   # like top for io
+        libinput \
+        maim xclip \
+        netcat \
+        openvpn \
+        pavucontrol \  # gui for pulseaudio
+        pulseaudio \  # for sound system
+        pulseaudio-alsa \  # pulseaudio alsa support
+        python-pip \
+        sudo \
+        unzip \
+        tilix \
+        ttf-bitstream-vera \
+        ttf-dejavu \
+        xbindkeys \
+        xnviewmp \
+        xorg-server \
+        xorg-xinit \
+        xorg-xinput \
+        xorg-xrandr \
+        vim \
+        vlc \  # media player
+        zsh
 fi
---
+++ arch/usbwakeup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Make USB ports wake from suspend
+# needed to allow wakeups by keyboard/mouse
+echo enabled > /sys/bus/usb/devices/usb1/power/wakeup
+echo enabled > /sys/bus/usb/devices/usb2/power/wakeup
+echo enabled > /sys/bus/usb/devices/usb3/power/wakeup
+echo enabled > /sys/bus/usb/devices/usb4/power/wakeup
+
+exit 0
